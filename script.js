f1f638a8804d77e70778c1da95e5201ff478d302
David Garcia <cardenete29@hotmail.com>
2024-08-21 17:55:51 +0200

    Cambios de pedro X(emoticono)

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function agregarALaPantalla(value) {
   }
   function deshacer() {
     const valorPantalla = document.getElementById('pantalla').value;
-    document.getElementById('pantalla').value = valorPantalla.slice(0,-1)
+    const resultado = valorPantalla.substring(0, valorPantalla.length -1);
+    document.getElementById('pantalla').value = resultado
   }
   
   
